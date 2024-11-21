dcf3991d2bb3c86dc0fe001631c32eb3d1f401d0
kaikbomfim <kaikbomfim@gmail.com>
2024-11-20 23:02:27 -0300

    fix: adjust services

--- src/app/services/categories/categories.service.ts
+++ src/app/services/categories/categories.service.ts
@@ -1,16 +1,17 @@
 import { Injectable } from '@angular/core';
 import { IItem } from '../../interfaces/item';
+import { environment } from '../../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CategoriesService {
-  url = 'http://localhost:3000/categories';
+  private baseUrl = environment.apiUrl;
 
   constructor() { }
 
   async getAllCategories(): Promise<IItem[]> {
-    const response = await fetch(this.url);
+    const response = await fetch(`${this.baseUrl}/categories`);
     return response.json();
   }
 
@@ -20,7 +21,7 @@ export class CategoriesService {
   // }
 
   async getCategoryById(id: number): Promise<IItem | undefined> {
-    const response = await fetch(`${this.url}?id=${id}`);
+    const response = await fetch(`${this.baseUrl}?id=${id}`);
     const categories = await response.json();
     return categories.length ? categories[0] : undefined;
   }
--- src/app/services/products/products.service.ts
+++ src/app/services/products/products.service.ts
@@ -1,27 +1,28 @@
 import { Injectable } from '@angular/core';
 import { IItem } from '../../interfaces/item';
+import { environment } from '../../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductsService {
-  url = 'http://localhost:3000/products';
+  private baseUrl = environment.apiUrl;
 
   constructor() { }
 
   async getAllProducts(): Promise<IItem[]> {
-    const response = await fetch(this.url);
+    const response = await fetch(this.baseUrl);
     return response.json();
   }
 
   async getProductById(id: number): Promise<IItem> {
-    const response = await fetch(`${this.url}?id=${id}`);
+    const response = await fetch(`${this.baseUrl}?id=${id}`);
     const products = await response.json();
     return products.length ? products[0] : undefined;
   }
 
   async getProductsByCategory(categoryId: number): Promise<IItem[]> {
-    const response = await fetch(`${this.url}?category=${categoryId}`);
+    const response = await fetch(`${this.baseUrl}?category=${categoryId}`);
     return response.json();
   }
 }
